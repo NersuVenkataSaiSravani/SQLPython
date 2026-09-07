5f48b97d16e5aaa5a7786b495e465a6a700f8726
NersuVenkataSaiSravani <147939468+NersuVenkataSaiSravani@users.noreply.github.com>
2026-09-07 14:49:17 +0530

    Enhance comments for database connection and cursor
    Added comments to clarify database connection and cursor usage.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import mysql.connector
-
+#Connection = connection to database
+#Cursor = performs database operations through that connection
 connection=mysql.connector.connect(  
       host="mysql",
       username="admin",
       password="rdscnt1231",
       database="databases3"
 )
-cursor=connection.cursor()
+cursor=connection.cursor() #A cursor is an object that lets your Python program send SQL commands to the database and read the results. Think of it like a messenger between Python and MySQL.
 
 cursor.execute("SELECT * FROM Employees")
 
